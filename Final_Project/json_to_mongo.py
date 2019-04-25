a55466cc4547b6dabf07f0478de785b8f3d773ce
Riley Shea <RileyMShea@gmail.com>
2019-04-24 20:09:32 -0400

    added function to create index changed json_to_mongo to act on main rochester_osm database

--- Final_Project/json_to_mongo.py
+++ Final_Project/json_to_mongo.py
@@ -9,11 +9,11 @@ from timeit import timeit
 from pprint import pprint
 
 
-
 def add_index(col: Collection):
     col.create_index([('id', ASCENDING),
                       ('type', DESCENDING)], unique=True, name='id_type_unique_index')
     return col.list_indexes()
+
 
 def json_to_mongo(json_file: str, col: Collection) -> List[InsertManyResult]:
     # makes a fresh collection from a json file
@@ -31,6 +31,23 @@ def get_db(db_name: str) -> Database:
     return db
 
 
+def get_user_counts(col: Collection):
+    user_counts_dict = col.aggregate([
+        {
+            '$sortByCount': '$user'
+        }, {
+            '$sort': {
+                'count': 1
+            }
+        }
+    ])
+    single_doc_user = list()
+    top_ten_users = list()
+    for entry in user_counts_dict:
+        if entry['count'] == 1:
+           single_doc_user.append(entry['user'])
+
+
 def main():
     json_file = "rochester_osm.json"
     db = get_db("udacity")  # type: Database
@@ -39,5 +56,7 @@ def main():
     total = sum(len(x.inserted_ids) for x in result)
     print(f"{total} records inserted from {json_file}")
     pprint(list(add_index(col=col)))
+
+
 if __name__ == "__main__":
     print(f"Completed in {timeit(main, number=1)} seconds")
