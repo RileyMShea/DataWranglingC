bce78031515995c43a0042ca7437462da3e876a4
Riley Shea <RileyMShea@gmail.com>
2019-05-02 16:39:41 -0400

    create and clean verified to be working with sample db
    Signed-off-by: Riley Shea <RileyMShea@gmail.com>

--- Final_Project/create_and_clean.py
+++ Final_Project/create_and_clean.py
@@ -88,14 +88,316 @@ def get_col(db_name="udacity", collection="rochester_osm"):
 def main():
     """currently set to use sample extract, for full implementation see jupyter notebook version of this file"""
     pprint("Processing dataset.. ")
-    file_name = r"rochester_sample.osm"
+
+    file_name = r"rochester_sample.osm"  # change name if using full extract
+
+    base = file_name.split('.')[0]
     osm_file = read_osm_file(filename=file_name)
     tag_list = ['node', 'way']
     result_set_list = get_soup(file=osm_file, tags=tag_list)
     osm_dicts = [get_dict_data(res.items()) for res in result_set_list]
     json_osm = json.dumps(osm_dicts)
-    with open("rochester_sample.json", 'w') as f:
+    with open(f"{base}.json", 'w') as f:
         f.write(json_osm)
+    from importlib import import_module
+
+    j2m = import_module('json_to_mongo')
+    j2m.main(json_file=f"{base}.json")
+
+    # setup connection for data exploration and cleaning
+    osm_col = get_col(collection=base)  # type: MongoClient
+
+    # ### Query total document count
+
+    # In[134]:
+
+    total_docs = osm_col.count_documents({})
+    pprint(total_docs)
+
+    # ### Get count of each key in collection
+
+    # In[135]:
+
+    key_counts_dict = dict()
+    for entry in tqdm(osm_col.find(), total=total_docs):
+        for k in entry.keys():
+            key_counts_dict.setdefault(k, 0)
+            key_counts_dict[k] += 1
+
+    # In[136]:
+
+    [print(f"{k}, {v} ") for k, v in key_counts_dict.items() if ':city' in k]
+
+    # In[137]:
+
+    # itemgetter used with sorted to allow sorting by key values
+    from operator import itemgetter
+
+    pprint(sorted(key_counts_dict.items(), key=itemgetter(1), reverse=True))
+
+    # ### Get a list of fields that begin with address
+
+    # In[138]:
+
+    address_fields = {k: v for (k, v) in key_counts_dict.items() if 'addr' in k}
+    pprint(sorted(address_fields.items(), key=itemgetter(1), reverse=True))
+
+    # In[139]:
+
+    # Get a list of distinct streets
+    distinct_streets = osm_col.distinct('addr:street')
+
+    # In[140]:
+
+    change_needed = list()
+
+    mapping_dict = get_suffix_mapping()
+    distinct_suffix = set(x.split()[-1] for x in distinct_streets)
+
+    # In[164]:
+
+    pprint(mapping_dict[0:5])
+
+    # In[142]:
+
+    for suffix in distinct_suffix:
+        for mapping in mapping_dict:
+            if mapping['Common'] == suffix.upper():
+                print(f"Changing {suffix} to {mapping['Standard']}")
+                continue
+
+    # In[143]:
+
+    modified_count = 0
+    for entry in tqdm(distinct_streets):
+        suffix = entry.split()[0]
+        for mapping in mapping_dict:
+            if mapping['Common'] == suffix.upper():
+                # print(f"Changing {suffix} to {mapping['Standard']}")
+                # print(entry.replace(suffix, mapping['Standard']))
+                result = osm_col.update_many({'addr:street': entry},
+                                             {"$set": {'addr:street': entry.replace(suffix, mapping['Standard'])}})
+                modified_count += result.modified_count
+                continue
+    print(f"{modified_count} address suffixes updated")
+
+    # In[144]:
+
+    # Get a list of distinct street types
+    pprint(set(x.split()[-1] for x in distinct_streets if x.split()[-1].isalpha()))
+
+    # In[145]:
+
+    # Get a list of distinct street types
+    pprint(set(x.split()[-1] for x in distinct_streets))
+
+    # ### find all address codes in collection
+
+    # In[146]:
+
+    unique_zip_codes = osm_col.distinct('addr:postcode')
+    pprint(unique_zip_codes)
+
+    # In[147]:
+
+    update_dict = {'modified': 0,
+                   'deleted': 0,
+                   'good': 0}
+    for zip in tqdm(unique_zip_codes):
+        if zip[0:5].isdigit() and len(zip) > 5:
+            result = osm_col.update_many({'addr:postcode': zip}, {"$set": {'addr:postcode': zip[0:5]}})
+            update_dict['modified'] += result.modified_count
+        elif not zip.isdigit() and len(zip) != 5:
+            result = osm_col.delete_many({'addr:postcode': zip})
+            update_dict['deleted'] += result.deleted_count
+        elif zip.isdigit() and len(zip) == 5:
+            update_dict['good'] += 1
+
+    pprint(update_dict)
+
+    # In[148]:
+
+    #
+    updated_address_code_list = list(osm_col.find({'addr:postcode': {'$exists': True}}, {'addr:postcode': 1, '_id': 0}))
+    set([x['addr:postcode'] for x in updated_address_code_list])
+
+    # [Rochester Zip codes](https://www.zip-codes.com/city/ny-rochester.asp)
+    # > After running our function we can see that all the unique zip codes in the database are valid Rochester Zip codes
+
+    # In[149]:
+
+    pprint(list(osm_col.aggregate([
+        {
+            '$group': {
+                '_id': '$addr:postcode',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$sort': {
+                'count': -1
+            }
+        }
+    ]))[:10])
+
+    # # User Counts
+
+    # In[150]:
+
+    def get_single_users(col: Collection):
+        user_counts_dict = list(col.aggregate([
+            {
+                '$sortByCount': '$user'
+            }, {
+                '$sort': {
+                    'count': 1
+                }
+            }
+        ]))
+        single_doc_user = list()
+        for entry in user_counts_dict:
+            if entry['count'] == 1:
+                single_doc_user.append(entry['_id'])
+            else:
+                break
+        pprint(single_doc_user[0:5])
+        pprint(f"{len(single_doc_user)} users with only one post out of {len(user_counts_dict)}")
+        # return single_doc_user
+
+    get_single_users(osm_col)
+
+    # In[151]:
+
+    user_df = pd.DataFrame.from_dict(list(osm_col.aggregate([{
+        '$sortByCount': '$user'
+    }])))
+    user_df['percent'] = user_df['count'] / user_df['count'].sum()
+
+    # In[152]:
+
+    # Percent of entries that came from top two users
+    user_df[0:2]['percent'].sum() * 100
+
+    # In[153]:
+
+    # Combined top 10 users contribution
+    user_df[0:10]['percent'].sum() * 100
+
+    # In[154]:
+
+    # Combined perecent of users who individually contribute less then 1% of the entries in the database
+    user_df[user_df.percent <= .01].percent.sum() * 100
+
+    # In[155]:
+
+    user_df._id
+
+    # In[156]:
+
+    def top_ten_amenities(col: Collection):
+        top_amenities = list(col.aggregate([
+            {
+                '$match': {
+                    'type': 'way'
+                }
+            }, {
+                '$sortByCount': '$amenity'
+            }
+        ]))
+        return top_amenities
+
+    top_ten_amenities(col=osm_col)[0:10]
+
+    # In[157]:
+
+    df = pd.DataFrame.from_dict(top_ten_amenities(osm_col))
+    pprint(df[0:10])
+
+    # In[158]:
+
+    df['percent'] = df['count'] / df['count'].sum()
+
+    # ``` table
+    # _id|count|percent\r\n|49953|0.951069055461417\r\nparking|1828|0.034803800239894905\r\nrestaurant|127|0.0024179883098832894\r\nschool|85|0.001618338632599052\r\nfuel|56|0.0010661995697123165\r\n
+    # ```
+
+    # In[159]:
+
+    df[1:].describe()
+
+    # In[160]:
+
+    df.shape[0]
+
+    # In[161]:
+
+    # Biggest Religion
+    religion = list(osm_col.aggregate([
+        {
+            '$match': {
+                'amenity': {
+                    '$eq': 'place_of_worship'
+                }
+            }
+        }, {
+            '$group': {
+                '_id': '$religion',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$sort': {
+                'count': -1
+            }
+        }
+    ]))
+    pprint(religion)
+
+    # In[162]:
+
+    # Most popular cuisine in restaurants
+    cuisine = list(osm_col.aggregate([
+        {
+            '$match': {
+                'amenity': {
+                    '$eq': 'restaurant'
+                }
+            }
+        }, {
+            '$group': {
+                '_id': '$cuisine',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$sort': {
+                'count': -1
+            }
+        }
+    ]))
+    pprint(cuisine[0:10])
+
+    # In[163]:
+
+    # City counts
+    city_counts = list(osm_col.aggregate([
+        {
+            '$group': {
+                '_id': '$addr:city',
+                'count': {
+                    '$sum': 1
+                }
+            }
+        }, {
+            '$sort': {
+                'count': -1
+            }
+        }
+    ]))
+    print('\n'.join('{_id!s:<20}{count}'.format(**x) for x in city_counts))
 
 
 if __name__ == '__main__':
@@ -105,338 +407,3 @@ if __name__ == '__main__':
 # - Create compound unique index on 'id' and 'type' fields
 
 # read the json file we just read to verify it's working
-from importlib import import_module
-
-j2m = import_module('json_to_mongo')
-j2m.main(json_file="rochester_sample.json")
-
-# setup connection for data exploration and cleaning
-osm_col = get_col(collection="rochester_sample")  # type: MongoClient
-
-# ### Query total document count
-
-# In[134]:
-
-
-total_docs = osm_col.count_documents({})
-pprint(total_docs)
-
-# ### Get count of each key in collection
-
-# In[135]:
-
-
-key_counts_dict = dict()
-for entry in tqdm(osm_col.find(), total=total_docs):
-    for k in entry.keys():
-        key_counts_dict.setdefault(k, 0)
-        key_counts_dict[k] += 1
-
-# In[136]:
-
-
-[print(f"{k}, {v} ") for k, v in key_counts_dict.items() if ':city' in k]
-
-# In[137]:
-
-
-# itemgetter used with sorted to allow sorting by key values
-from operator import itemgetter
-
-pprint(sorted(key_counts_dict.items(), key=itemgetter(1), reverse=True))
-
-# ### Get a list of fields that begin with address
-
-# In[138]:
-
-
-address_fields = {k: v for (k, v) in key_counts_dict.items() if 'addr' in k}
-pprint(sorted(address_fields.items(), key=itemgetter(1), reverse=True))
-
-# In[139]:
-
-
-# Get a list of distinct streets
-distinct_streets = osm_col.distinct('addr:street')
-
-# In[140]:
-
-
-change_needed = list()
-
-mapping_dict = get_suffix_mapping()
-distinct_suffix = set(x.split()[-1] for x in distinct_streets)
-
-# In[164]:
-
-
-pprint(mapping_dict[0:5])
-
-# In[142]:
-
-
-for suffix in distinct_suffix:
-    for mapping in mapping_dict:
-        if mapping['Common'] == suffix.upper():
-            print(f"Changing {suffix} to {mapping['Standard']}")
-            continue
-
-# In[143]:
-
-
-modified_count = 0
-for entry in tqdm(distinct_streets):
-    suffix = entry.split()[0]
-    for mapping in mapping_dict:
-        if mapping['Common'] == suffix.upper():
-            # print(f"Changing {suffix} to {mapping['Standard']}")
-            # print(entry.replace(suffix, mapping['Standard']))
-            result = osm_col.update_many({'addr:street': entry},
-                                         {"$set": {'addr:street': entry.replace(suffix, mapping['Standard'])}})
-            modified_count += result.modified_count
-            continue
-print(f"{modified_count} address suffixes updated")
-
-# In[144]:
-
-
-# Get a list of distinct street types
-pprint(set(x.split()[-1] for x in distinct_streets if x.split()[-1].isalpha()))
-
-# In[145]:
-
-
-# Get a list of distinct street types
-pprint(set(x.split()[-1] for x in distinct_streets))
-
-# ### find all address codes in collection
-
-# In[146]:
-
-
-unique_zip_codes = osm_col.distinct('addr:postcode')
-pprint(unique_zip_codes)
-
-# In[147]:
-
-
-update_dict = {'modified': 0,
-               'deleted': 0,
-               'good': 0}
-for zip in tqdm(unique_zip_codes):
-    if zip[0:5].isdigit() and len(zip) > 5:
-        result = osm_col.update_many({'addr:postcode': zip}, {"$set": {'addr:postcode': zip[0:5]}})
-        update_dict['modified'] += result.modified_count
-    elif not zip.isdigit() and len(zip) != 5:
-        result = osm_col.delete_many({'addr:postcode': zip})
-        update_dict['deleted'] += result.deleted_count
-    elif zip.isdigit() and len(zip) == 5:
-        update_dict['good'] += 1
-
-pprint(update_dict)
-
-# In[148]:
-
-
-#
-updated_address_code_list = list(osm_col.find({'addr:postcode': {'$exists': True}}, {'addr:postcode': 1, '_id': 0}))
-set([x['addr:postcode'] for x in updated_address_code_list])
-
-# [Rochester Zip codes](https://www.zip-codes.com/city/ny-rochester.asp)
-# > After running our function we can see that all the unique zip codes in the database are valid Rochester Zip codes
-
-# In[149]:
-
-
-pprint(list(osm_col.aggregate([
-    {
-        '$group': {
-            '_id': '$addr:postcode',
-            'count': {
-                '$sum': 1
-            }
-        }
-    }, {
-        '$sort': {
-            'count': -1
-        }
-    }
-]))[:10])
-
-
-# # User Counts
-
-# In[150]:
-
-
-def get_single_users(col: Collection):
-    user_counts_dict = list(col.aggregate([
-        {
-            '$sortByCount': '$user'
-        }, {
-            '$sort': {
-                'count': 1
-            }
-        }
-    ]))
-    single_doc_user = list()
-    for entry in user_counts_dict:
-        if entry['count'] == 1:
-            single_doc_user.append(entry['_id'])
-        else:
-            break
-    pprint(single_doc_user[0:5])
-    pprint(f"{len(single_doc_user)} users with only one post out of {len(user_counts_dict)}")
-    # return single_doc_user
-
-
-get_single_users(osm_col)
-
-# In[151]:
-
-
-user_df = pd.DataFrame.from_dict(list(osm_col.aggregate([{
-    '$sortByCount': '$user'
-}])))
-user_df['percent'] = user_df['count'] / user_df['count'].sum()
-
-# In[152]:
-
-
-# Percent of entries that came from top two users
-user_df[0:2]['percent'].sum() * 100
-
-# In[153]:
-
-
-# Combined top 10 users contribution
-user_df[0:10]['percent'].sum() * 100
-
-# In[154]:
-
-
-# Combined perecent of users who individually contribute less then 1% of the entries in the database
-user_df[user_df.percent <= .01].percent.sum() * 100
-
-# In[155]:
-
-
-user_df._id
-
-
-# In[156]:
-
-
-def top_ten_amenities(col: Collection):
-    top_amenities = list(col.aggregate([
-        {
-            '$match': {
-                'type': 'way'
-            }
-        }, {
-            '$sortByCount': '$amenity'
-        }
-    ]))
-    return top_amenities
-
-
-top_ten_amenities(col=osm_col)[0:10]
-
-# In[157]:
-
-
-df = pd.DataFrame.from_dict(top_ten_amenities(osm_col))
-pprint(df[0:10])
-
-# In[158]:
-
-
-df['percent'] = df['count'] / df['count'].sum()
-
-# ``` table
-# _id|count|percent\r\n|49953|0.951069055461417\r\nparking|1828|0.034803800239894905\r\nrestaurant|127|0.0024179883098832894\r\nschool|85|0.001618338632599052\r\nfuel|56|0.0010661995697123165\r\n
-# ```
-
-# In[159]:
-
-
-df[1:].describe()
-
-# In[160]:
-
-
-df.shape[0]
-
-# In[161]:
-
-
-# Biggest Religion
-religion = list(osm_col.aggregate([
-    {
-        '$match': {
-            'amenity': {
-                '$eq': 'place_of_worship'
-            }
-        }
-    }, {
-        '$group': {
-            '_id': '$religion',
-            'count': {
-                '$sum': 1
-            }
-        }
-    }, {
-        '$sort': {
-            'count': -1
-        }
-    }
-]))
-pprint(religion)
-
-# In[162]:
-
-
-# Most popular cuisine in restaurants
-cuisine = list(osm_col.aggregate([
-    {
-        '$match': {
-            'amenity': {
-                '$eq': 'restaurant'
-            }
-        }
-    }, {
-        '$group': {
-            '_id': '$cuisine',
-            'count': {
-                '$sum': 1
-            }
-        }
-    }, {
-        '$sort': {
-            'count': -1
-        }
-    }
-]))
-pprint(cuisine[0:10])
-
-# In[163]:
-
-
-# City counts
-city_counts = list(osm_col.aggregate([
-    {
-        '$group': {
-            '_id': '$addr:city',
-            'count': {
-                '$sum': 1
-            }
-        }
-    }, {
-        '$sort': {
-            'count': -1
-        }
-    }
-]))
-print('\n'.join('{_id!s:<20}{count}'.format(**x) for x in city_counts))
-
-# In[ ]:
--- Submisssion/README.md
+++ Submisssion/README.md
@@ -1,41 +1,55 @@
-###Files included
+### Files included
 
 Submission
 
 1. A pdf document containing your answers to the rubric questions. This file should document your data wrangling process.
 >  `test_scientific_export.pdf`
 2. Your Python code you used in auditing and cleaning your dataset for the final project. You may also include the original Case Study scripts, but make sure it is clear which code was used for the project, and which applies to the lesson quizzes. We recommend including a Readme file to describe the contents of each file you include in your submission.
+
 `README.md`
 > Current file: describing other files in submission
+
 `final_submission.ipynb`
 > Jupyter Notebook(Lab) file containing answers to rubric questions
+
 `test_scientific_export.ipynb`
 > Jupyter Notebook containing most code necessary to fulfill project requirements
+
 `json_to_mongo.py`
 > python file containing functions used in `test_scientific_export.ipynb` above
 `audit.py`
 > adapted audit code from lessons not used in final project
+
 `audit_street_names.py`
 > Code from lessons that could've been used to audit street names
+
 `data.py`
 > Code from lessons that could've been used 
+
 `mapparser.py`
 > Code from lessons for seeing tag types in OSM file
+
 `rochester_osm.json`
-> json OSM converted from XML OSM file
+> json OSM converted from XML OSM file.  Referenced in Jupyter notebook but not included in submission due to size.
+
 `osm_sample_maker.py`
 > Code from Project description used to generate the sample OSM file
+
 `users.py`
 > Code from Lessons used to find unique users
+
 `rochester_ny.osm`
 > Primary osm data file(not included in submission due to size)
+
 `rochester_sample.osm`
 >  Sample of full Rochester osm file created through processing `rochester_ny.osm` with `osm_sample_maker.py`
 `environment.yml`
+
 > Conda environment used for project. Python 3.7 was used since support for 2.7 is being discontinued. Environment can be recreated with `conda create --name` **MyEnvironment**`--file environment.yml`
-3. a text file containing a link to the map position you wrangled in your project, a short description of the area and a reason for your choice.
+
+3. A text file containing a link to the map position you wrangled in your project, a short description of the area and a reason for your choice.
 > `map_area.txt`
-4. an .osm file containing a sample part of the map region you used (around 1 - 10 mb in size). see the notes below.
+4. An .osm file containing a sample part of the map region you used (around 1 - 10 mb in size). see the notes below.
 > `rochester_sample.osm`
-5. a text file containing a list of web sites, books, forums, blog posts, github repositories etc that you referred to or used in this submission (add n/a if you did not use such resources).
+5. A text file containing a list of web sites, books, forums, blog posts, github repositories etc that you referred to or used in this submission (add n/a if you did not use such resources).
 `resources used.txt`
